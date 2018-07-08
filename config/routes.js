7157b928cfbf111faee4e4b90ef8b39d4fd76b61
David Solorzano <abdsp22@gmail.com>
2018-07-08 03:35:50 -0400

    change controller history medical

--- config/routes.js
+++ config/routes.js
@@ -12,8 +12,10 @@ module.exports.routes = {
 
   'GET /products/find': { controller: 'ProductsController', action: 'find'},
   'GET /patients/find': { controller: 'PatientsController', action: 'getPatients'},
-  'GET /MedicalConsultation/find': { controller: 'ConsultationsController', action: 'find'},
+  'GET /MedicalConsultation/find': { controller: 'MedicalHistoryController', action: 'find'},
   'POST /reminders/send': { controller: 'RemindersController', action: 'sendNotification'},
+  //'GET /MedicalHistory/find': { controller: 'MedicalHistoryController', action: 'find'},
+
 
 
 
