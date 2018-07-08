80f8f6caf362c1557cf01e9a849c86afc9c2ac04
David Solorzano <abdsp22@gmail.com>
2018-07-08 01:30:14 -0400

    new model data

--- api/models/MedicalConsultation.js
+++ api/models/MedicalConsultation.js
@@ -9,18 +9,18 @@ module.exports = {
 
   attributes: {
     
-    rut: {type: 'number', required: true},
-    specialty: {type: 'string', required:true},
-    medication: {type: 'number', required: true},
-    symptom: {type: 'json', required: true},
-    patology: {type: 'json', required: true},
-    medication: {type: 'json', required: true},
-    exams: {type: 'json', required: true},
-    indications: {type: 'string', required: true},
-    prices: {type: 'number', required: true},
-    case_medical: {type:'string', required: true},
-    createdAt: { type: 'string', autoCreatedAt: true },
-    updatedAt: { type: 'string', autoUpdatedAt: true },
+    //rut: {type: 'number', required: true},
+    speciality: {type: 'json', required:true},
+    //case_medical: {type: 'number', required: true},
+    //symptom: {type: 'json', required: true},
+    //patology: {type: 'json', required: true},
+    //medication: {type: 'json', required: true},
+    //exams: {type: 'json', required: true},
+    //indications: {type: 'string', required: true},
+    //prices: {type: 'number', required: true},
+    //case_medical: {type:'string', required: true},
+    //createdAt: { type: 'string', autoCreatedAt: true },
+    //updatedAt: { type: 'string', autoUpdatedAt: true },
 
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
